22101db1d3c3e54aa331a6ab037abcaa671e3470
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-01-05 15:02:27 +0800

    Changed from Future<void> to Future

--- lib/repository/reactive_todos_repository.dart
+++ lib/repository/reactive_todos_repository.dart
@@ -1,11 +1,11 @@
 import 'package:todo_changenotifier/models/models.dart';
 
 abstract class ReactiveTodosRepository {
-  Future<void> addTodo(Todo todo);
+  Future addTodo(Todo todo);
 
-  Future<void> deleteTodos(List<String> todoIds);
+  Future deleteTodos(List<String> todoIds);
 
   Stream<List<Todo>> todos();
 
-  Future<void> updateTodo(Todo todo);
+  Future updateTodo(Todo todo);
 }
